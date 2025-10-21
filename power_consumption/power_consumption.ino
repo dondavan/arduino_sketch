d884b181b2c6c5dd5024d6302c8716b688552f18
dondavan <yeezushugh@gmail.com>
2025-10-21 16:34:24 +0200

    library

--- power_consumption/power_consumption.ino
+++ power_consumption/power_consumption.ino
@@ -3,7 +3,7 @@
 Adafruit_INA260 ina260 = Adafruit_INA260();
 
 void setup() {
-  Serial.begin(19200);
+  Serial.begin(9600);
   // Wait until serial port is opened
   while (!Serial) { delay(10); }
 
