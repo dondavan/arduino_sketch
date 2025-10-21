1e9ff6ed3b5b2f59fde2a60e8b441ee38032ef67
dondavan <yeezushugh@gmail.com>
2025-10-21 16:07:21 +0200

    library

--- serial_monitor.sh
+++ serial_monitor.sh
@@ -1,2 +1,3 @@
 sudo chmod a+rw /dev/ttyACM0
-cat /dev/ttyACM0
+cat < /dev/ttyACM0 | awk 'BEGIN { FS = "," } {OFMT = "%2.5f"}\
+/GPGGA/ { print "ID: ", $1, "Time: ", $2, "Lat: ", $3, "Lon: ", $5, "Höhe: ", $10}' > data
