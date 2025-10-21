3e90ff17149fcc6ead837870e788d824a6608206
dondavan <yeezushugh@gmail.com>
2025-10-21 15:48:38 +0200

    library

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-./bin/arduino-cli lib install Adafruit INA260 Library
+./bin/arduino-cli lib install Adafruit_INA260_Library
 ./bin/arduino-cli compile --fqbn arduino:avr:uno power_consumption --build-path=./build
