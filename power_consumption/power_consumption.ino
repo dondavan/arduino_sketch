2d1070a212c037ab5a103b53c27bd42a84638f29
dondavan <yeezushugh@gmail.com>
2025-10-22 14:04:39 +0200

    pin

--- power_consumption/power_consumption.ino
+++ power_consumption/power_consumption.ino
@@ -33,8 +33,8 @@ void loop() {
   }
   */
 
-  Serial.print(micros());
-  //Serial.write(ina260.readPower());
+  Serial.println(micros());
+  Serial.println(ina260.readPower());
 
   //delay(10);
 }
